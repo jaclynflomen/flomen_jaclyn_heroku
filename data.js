7375dad6f9a4ed39d291dff98605dafbad389229
jaclynflomen <j_floman@fanshaweonline.ca>
2019-03-21 22:48:10 -0400

    adding data

--- data.js
+++ data.js
@@ -5,9 +5,9 @@ const data = {
     user1: {
         name: "AirPods",
         brand: "Apple",
-        description: "some bio info goes here",
-        features: "",
-        bioPic: "apple.png"
+        description: "After a simple one-tap setup, AirPods are automatically on and always connected. Using them is just as easy. They sense when they’re in your ears and pause when taking them out. The AirPods experience is as amazing with any Apple product!",
+        features: "Wireless charging case, Bluetooth 'Hey, Siri' connectivity, a faster connection than ever before, 24 hour battery life, instant setup.",
+        bioPic: "airpods.jpeg"
     }
 };
 
